cb3ee80bdbc3faa1980d894ae3f0e5302a6e61cd
Frederic Fays <frederic@fays.eu>
2016-06-20 16:02:06 +0200

    Improved

--- src/main/java/eu/fays/sandbox/jaxb/ListItem.java
+++ src/main/java/eu/fays/sandbox/jaxb/ListItem.java
@@ -1,10 +1,8 @@
 package eu.fays.sandbox.jaxb;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlValue;
 
 /**
@@ -13,32 +11,21 @@ import javax.xml.bind.annotation.XmlValue;
  * @author Frederic Fays
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-public class ListItem<T> {
-
-	/**
-	 * Constructor
-	 */
-	public ListItem() {
-		try {
-			_li = newLi();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-
-		}
-	}
-
-	/**
-	 * Constructor
-	 * @param li value of the list item
-	 */
-	public ListItem(T li) {
-		_li = li;
-	}
+public abstract class ListItem<T> {
 
 	/**
 	 * Returns the value
 	 * @return the value
 	 */
 	public T getLi() {
+		if (_li == null) {
+			try {
+				_li = getParameterizedType().newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
 		return _li;
 	}
 
@@ -51,20 +38,11 @@ public class ListItem<T> {
 	}
 
 	/**
-	 * Instanciate a new list item:<br>
-	 * <br>
-	 * Article: <a href="http://stackoverflow.com/questions/3437897/how-to-get-a-class-instance-of-generics-type-t">How to get a class instance of generics type T</a><br>
-	 * @return the new list item
-	 * @throws ClassNotFoundException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
+	 * Returns the effective class of the parameterized type
+	 * @return the class
 	 */
-	@SuppressWarnings("unchecked")
-	private T newLi() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-		final Type gsc = getClass().getGenericSuperclass();
-		final Type t = ((ParameterizedType) gsc).getActualTypeArguments()[0];
-		return (T) (Class.forName(t.toString()).newInstance());
-	}
+	@XmlTransient
+	public abstract Class<T> getParameterizedType();
 
 	/**
 	 * @see java.lang.Object#hashCode()
