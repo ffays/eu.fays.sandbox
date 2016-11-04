f13040c206eb0c4247bd653e138401e35385d2bc
Frederic Fays <frederic@fays.eu>
2016-11-04 11:29:44 +0100

    Fixed issue

--- src/main/java/eu/fays/sandbox/jaxb/MySubData.java
+++ src/main/java/eu/fays/sandbox/jaxb/MySubData.java
@@ -43,6 +43,6 @@ public class MySubData {
 	// NCName definition: https://www.w3.org/TR/1999/WD-xmlschema-2-19990924/#NCName
 	// Name definition : https://www.w3.org/TR/REC-xml/#d0e804
 	// U+01C0 : LATIN LETTER DENTAL - http://www.fileformat.info/info/unicode/char/01c0/index.htm
-	private String myId = "id:u01C0" + UUID.randomUUID().toString();
+	private String myId = "id\u01C0" + UUID.randomUUID().toString();
 
 }
