0d37ee2695d4b2533b165f18105e76508a77a0fe
Frederic Fays <frederic@fays.eu>
2016-11-09 17:39:44 +0100

    Added a regex pattern

--- src/main/java/eu/fays/sandbox/jaxb/MySubData.java
+++ src/main/java/eu/fays/sandbox/jaxb/MySubData.java
@@ -45,4 +45,6 @@ public class MySubData {
 	// U+01C0 : LATIN LETTER DENTAL - http://www.fileformat.info/info/unicode/char/01c0/index.htm
 	private String myId = "id\u01C0" + UUID.randomUUID().toString();
 
+	/** Invalid characters for NCName (i.e. IDREF) */
+	public static final String NCNAME_INVALID_CHARACTER_REGEX = "[^\\p{Alnum}_\\-.\\u00B7\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0300-\\u036F\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u203F-\\u2040\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD]";
 }
