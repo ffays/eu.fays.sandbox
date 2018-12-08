9a7cd96490b489e131982e55cbd171b98b98f3d3
Frédéric Fays <frederic@fays.eu>
2018-12-08 18:36:32 +0100

    Improved version of JAXB map of map essay #3

--- src/main/java/eu/fays/sandbox/jaxb/mapofmap3/Document.java
+++ src/main/java/eu/fays/sandbox/jaxb/mapofmap3/Document.java
@@ -1,15 +1,10 @@
 package eu.fays.sandbox.jaxb.mapofmap3;
 
 import static java.lang.Boolean.TRUE;
-import static java.text.MessageFormat.format;
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 import static javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT;
 
 import java.io.OutputStream;
-import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -101,39 +96,29 @@ public class Document {
 		assert out != null;
 		//
 
-		// @formatter:off
-		final Map<Boolean, List<Entry<String, Object>>> collect = getDictionary().entrySet().stream().collect(groupingBy(e -> e.getValue() instanceof Map));		
-		// @formatter:on
+		if (dictionary != null && !dictionary.isEmpty()) {
+			final Map<String, Map<String, Object>> maps = new LinkedHashMap<>();
+			final Map<String, Object> map0 = new LinkedHashMap<>();
+			maps.put(null, map0);
+			for (Entry<String, Object> entry : dictionary.entrySet()) {
+				if (entry.getValue() instanceof Map) {
+					@SuppressWarnings("unchecked")
+					final Map<String, Object> map1 = (Map<String, Object>) entry.getValue();
+					map0.put(entry.getKey(), map1);
+				} else {
+					map0.put(entry.getKey(), entry.getValue());
+				}
+			}
 
-		if (!collect.isEmpty()) {
-			final List<Dictionary> dictionaries = new ArrayList<Dictionary>();
-			final Map<String, Object> map0;
-			if (collect.containsKey(false)) {
-				// @formatter:off
-				map0 = collect.get(false).stream().collect(toMap(Entry::getKey, Entry::getValue, (k0, k1) -> { throw new AssertionError(format("Duplicate key ''{0}''!", k0)); }, LinkedHashMap<String, Object>::new));
-				// @formatter:on
-			} else {
-				map0 = new LinkedHashMap<>();
-			}
-			final Entry<String, Map<String, Object>> entry = new SimpleImmutableEntry<>(null, map0);
-			final Dictionary dictionary = new Dictionary(entry);
-			dictionaries.add(dictionary);
-			if (collect.containsKey(true)) {
-				// @formatter:off
-				@SuppressWarnings("unchecked")
-				final List<Dictionary> list = collect.get(true).stream()
-					.map(e -> new SimpleImmutableEntry<String, Map<String, Object>>(e.getKey(), (Map<String, Object>)e.getValue()))
-					.map(Dictionary::new)
-					.flatMap(Dictionary::dictionaryStream)
-					.collect(toList());
-				// @formatter::on
-				dictionaries.addAll(list);
-			}
-			
+			// @formatter:off
+			final List<Dictionary> dictionaries = maps.entrySet().stream()
+				.map(Dictionary::new)
+				.flatMap(Dictionary::dictionaryStream)
+				.collect(toList());
+			// @formatter::on
 
 			this.dictionaries = dictionaries;
 		}
-		
 		
 		final JAXBContext context = JAXBContext.newInstance(getClass());
 		final Marshaller marshaller = context.createMarshaller();
--- src/main/java/eu/fays/sandbox/jaxb/mapofmap3/JAXBMapOfMapEssay3.java
+++ src/main/java/eu/fays/sandbox/jaxb/mapofmap3/JAXBMapOfMapEssay3.java
@@ -5,11 +5,14 @@ import static java.lang.Math.PI;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toMap;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.IntStream;
 
 import eu.fays.sandbox.iterators.Fruit;
 
@@ -22,9 +25,20 @@ public class JAXBMapOfMapEssay3 {
 		dict1.put("E", E);
 		final Map<String, Object> dict2 = stream(Fruit.values())
 				.map(e -> new SimpleImmutableEntry<>(e.name(), e.ordinal()))
-				.collect(toMap(Entry::getKey, Entry::getValue));
+				.collect(toMap(Entry::getKey, Entry::getValue, (k0, k1) -> null, LinkedHashMap::new));
 		dict1.put(Fruit.class.getSimpleName(), dict2);
-		final Document document = new Document(now, dict1);
+
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM");
+		// @formatter:off
+		final Map<String, Integer> dict3 = IntStream
+			.rangeClosed(1, 12)
+			.mapToObj(m -> LocalDate.of(2001, m, 1))
+			.map(d -> new SimpleImmutableEntry<>(formatter.format(d), d.getMonthValue()))
+			.collect(toMap(Entry::getKey, Entry::getValue, (k0, k1) -> null, LinkedHashMap::new));
+		// @formatter:on
+		dict2.put("Months", dict3);
+
+		final Document document = new Document(JAXBMapOfMapEssay3.class.getSimpleName() + "_" + now, dict1);
 		document.marshal(System.out);
 	}
 }
