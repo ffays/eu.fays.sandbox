be9bb3847435df23406c59c0fb02c841691fd912
Frédéric Fays <frederic@fays.eu>
2020-10-03 13:34:01 +0200

    Use JUnit4 instead of JUnit5

--- src/test/java/eu/fays/sandbox/tree/NodeTest.java
+++ src/test/java/eu/fays/sandbox/tree/NodeTest.java
@@ -1,7 +1,7 @@
 package eu.fays.sandbox.tree;
 
 import static java.util.stream.Collectors.joining;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
