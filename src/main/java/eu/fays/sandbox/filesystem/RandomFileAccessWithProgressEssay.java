03d514fbc4880581a58937291d06adb3752cb894
Frederic Fays <frederic@fays.eu>
2021-12-15 08:46:11 +0100

    Javadoc + readAllBytes() method

--- src/main/java/eu/fays/sandbox/filesystem/RandomFileAccessWithProgressEssay.java
+++ src/main/java/eu/fays/sandbox/filesystem/RandomFileAccessWithProgressEssay.java
@@ -1,5 +1,6 @@
 package eu.fays.sandbox.filesystem;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,6 +9,13 @@ import java.nio.channels.Channels;
 import java.time.LocalDateTime;
 
 public class RandomFileAccessWithProgressEssay {
+
+	/**
+	 * Read the given file and display its reading progress to the console
+	 * 
+	 * @param args first argument is the file to be read
+	 * @throws IOException in case of unexpected error
+	 */
 	public static void main(String[] args) throws Exception {
 		final String filename = args[0];
 		final File file = new File(filename);
@@ -37,10 +45,31 @@ public class RandomFileAccessWithProgressEssay {
 			};
 
 			readProgressThread.start();
-
 			is.readAllBytes();
+//			readAllBytes(is);
 		}
 		System.out.println(LocalDateTime.now());
-
 	}
+
+	/**
+	 * Slow fashion to read all bytes of the given input stream
+	 * @param is input stream
+	 * @return all bytes
+   * @throws IOException if an I/O error occurs
+	 * @see InputStream#readAllBytes()
+	 */
+	public static byte[] readAllBytes(final InputStream is) throws IOException {
+		byte[] result = null;
+		try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			int c = is.read();
+			while (c != -1) {
+				baos.write(c);
+				c = is.read();
+			}
+			result = baos.toByteArray();
+		}
+
+		return result;
+	}
+
 }
