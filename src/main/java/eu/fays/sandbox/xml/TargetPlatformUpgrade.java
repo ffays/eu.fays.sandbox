e7537c1fc0efe0f839471d6c0466b19611549218
Frederic Fays <frederic@fays.eu>
2022-11-03 17:06:39 +0100

    TargetPratformUpdate two passes

--- src/main/java/eu/fays/sandbox/xml/TargetPlatformUpgrade.java
+++ src/main/java/eu/fays/sandbox/xml/TargetPlatformUpgrade.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -48,7 +49,7 @@ public class TargetPlatformUpgrade {
 		synonymsMap.put("org.apiguardian", "org.apiguardian.api");
 		
 		// Missing bundles 
-		final Map<String,Element> missingMap = new HashMap<>();
+		final Map<String,Element> missingMap = new LinkedHashMap<>();
 
 		// Repository content file dictionary
 		final Map<String, File> repositoryContentFileMap = new HashMap<>();
@@ -60,83 +61,102 @@ public class TargetPlatformUpgrade {
 		
 		if (!rootNode.hasChildNodes()) return;
 		
-		for (Node locationsElement = rootNode.getFirstChild(); locationsElement != null; locationsElement = locationsElement.getNextSibling()) {
-			if(!locationsElement.hasChildNodes()) continue;
-			if(!"locations".equals(locationsElement.getNodeName())) continue;
-			
-			for (Node locationElement = locationsElement.getFirstChild(); locationElement != null; locationElement = locationElement.getNextSibling()) {
-				if(!locationElement.hasChildNodes()) continue;
-				String repositoryUrl = null;
-				Node repositoryNode = null;
-				final LinkedHashMap<String, String> bundleVersionMap = new LinkedHashMap<>();
-				final LinkedHashMap<String, Element> unitElementMap = new LinkedHashMap<>();
-				for (Node childElement = locationElement.getFirstChild(); childElement != null; childElement = childElement.getNextSibling()) {
-					if("unit".equals(childElement.getNodeName())) {
-						final String bundleSymbolicName = childElement.getAttributes().getNamedItem("id").getNodeValue();
-						final String version = childElement.getAttributes().getNamedItem("version").getNodeValue();
-						bundleVersionMap.put(bundleSymbolicName, version);
-						unitElementMap.put(bundleSymbolicName, (Element) childElement);
-					} else if("repository".equals(childElement.getNodeName())) {
-						repositoryUrl = childElement.getAttributes().getNamedItem("location").getNodeValue();
-						repositoryNode = childElement;
-					}
-				}
-				final String currentRepositoryContentFilename = encode(repositoryUrl, UTF_8) + ".txt";
-				String newRepositoryContentFilename = null;
-				if(repositoryContentFileMap.containsKey(currentRepositoryContentFilename)) {
-					newRepositoryContentFilename = currentRepositoryContentFilename;
-				} else {
-					int best = -1;
-					for(final String repositoryContentFilename : repositoryContentFileMap.keySet()) {
-						int score = findFirstMismatch(currentRepositoryContentFilename, repositoryContentFilename);
-						if(score > best) {
-							best = score;
-							newRepositoryContentFilename = repositoryContentFilename;
-						}
-					}
-				}
-				assert newRepositoryContentFilename != null;
-				final String newRepositoryUrl = decode(newRepositoryContentFilename.substring(0, newRepositoryContentFilename.length()-4), UTF_8);
-				LOGGER.info(format("location {0} \u2794 {1}", repositoryUrl, newRepositoryUrl));
+		for(int pass=1; pass<=2; pass++) {
+			for (Node locationsElement = rootNode.getFirstChild(); locationsElement != null; locationsElement = locationsElement.getNextSibling()) {
+				if(!locationsElement.hasChildNodes()) continue;
+				if(!"locations".equals(locationsElement.getNodeName())) continue;
 				
-				final Properties bundleNewVersionMap = new Properties();
-				final File newRepositoryContentFile = repositoryContentFileMap.get(newRepositoryContentFilename);
-				try(final FileReader reader = new FileReader(newRepositoryContentFile, UTF_8)) {
-					bundleNewVersionMap.load(reader);
-					for(final Entry<String, Element> entry : unitElementMap.entrySet()) {
-						final String bundleSymbolicName = entry.getKey();
-						final Element unitElement = entry.getValue();
-						final String oldVersion = bundleVersionMap.get(bundleSymbolicName);
-						if(bundleNewVersionMap.containsKey(bundleSymbolicName)) {
-							final String newVersion = bundleNewVersionMap.getProperty(bundleSymbolicName);
-							unitElement.getAttributes().getNamedItem("version").setNodeValue(newVersion);
-							int upgraded = newVersion.equals(oldVersion)?0:newVersion.compareTo(oldVersion)>0?1:-1;
-							LOGGER.info(format("bundle{0,choice,0# |1#\u2191|2#?} {1} : {2} \u2794 {3}", upgraded, bundleSymbolicName, oldVersion, newVersion));
-						} else if (synonymsMap.containsKey(bundleSymbolicName) && bundleNewVersionMap.containsKey(synonymsMap.get(bundleSymbolicName))) {
-							final String newBundleSymbolicName = synonymsMap.get(bundleSymbolicName);
-							final String newVersion = bundleNewVersionMap.getProperty(newBundleSymbolicName);
-							unitElement.getAttributes().getNamedItem("id").setNodeValue(newBundleSymbolicName);
-							unitElement.getAttributes().getNamedItem("version").setNodeValue(newVersion);
-							int upgraded = newVersion.equals(oldVersion)?0:newVersion.compareTo(oldVersion)>0?1:-1;
-							LOGGER.info(format("bundle{0,choice,0# |1#\u2191|2#?} {1} \u2794 {2} : {3} \u2794 {4}", upgraded, bundleSymbolicName, newBundleSymbolicName, oldVersion, newVersion));
-						} else {
-							missingMap.put(bundleSymbolicName, unitElement);
-							unitElement.getParentNode().removeChild(unitElement);
+				for (Node locationElement = locationsElement.getFirstChild(); locationElement != null; locationElement = locationElement.getNextSibling()) {
+					if(!locationElement.hasChildNodes()) continue;
+					String repositoryUrl = null;
+					Node repositoryNode = null;
+					final LinkedHashMap<String, String> bundleVersionMap = new LinkedHashMap<>();
+					final LinkedHashMap<String, Element> unitElementMap = new LinkedHashMap<>();
+					for (Node childElement = locationElement.getFirstChild(); childElement != null; childElement = childElement.getNextSibling()) {
+						if("unit".equals(childElement.getNodeName())) {
+							final String bundleSymbolicName = childElement.getAttributes().getNamedItem("id").getNodeValue();
+							final String version = childElement.getAttributes().getNamedItem("version").getNodeValue();
+							bundleVersionMap.put(bundleSymbolicName, version);
+							unitElementMap.put(bundleSymbolicName, (Element) childElement);
+						} else if("repository".equals(childElement.getNodeName())) {
+							repositoryUrl = childElement.getAttributes().getNamedItem("location").getNodeValue();
+							repositoryNode = childElement;
 						}
 					}
+					final String currentRepositoryContentFilename = encode(repositoryUrl, UTF_8) + ".txt";
+					String newRepositoryContentFilename = null;
+					if(repositoryContentFileMap.containsKey(currentRepositoryContentFilename)) {
+						newRepositoryContentFilename = currentRepositoryContentFilename;
+					} else {
+						int best = -1;
+						for(final String repositoryContentFilename : repositoryContentFileMap.keySet()) {
+							int score = findFirstMismatch(currentRepositoryContentFilename, repositoryContentFilename);
+							if(score > best) {
+								best = score;
+								newRepositoryContentFilename = repositoryContentFilename;
+							}
+						}
+					}
+					assert newRepositoryContentFilename != null;
+					final String newRepositoryUrl = decode(newRepositoryContentFilename.substring(0, newRepositoryContentFilename.length()-4), UTF_8);
+					LOGGER.info(format("location {0} \u2794 {1}", repositoryUrl, newRepositoryUrl));
+					
+					final Properties bundleNewVersionProperties = new Properties();
+					final File newRepositoryContentFile = repositoryContentFileMap.get(newRepositoryContentFilename);
+					try(final FileReader reader = new FileReader(newRepositoryContentFile, UTF_8)) {
+						bundleNewVersionProperties.load(reader);
+						
+						if(pass == 1) {
+							// 1st pass : identify both upgraded bundles and missing bundles
+							for(final Entry<String, Element> entry : unitElementMap.entrySet()) {
+								final String bundleSymbolicName = entry.getKey();
+								final Element unitElement = entry.getValue();
+								final String oldVersion = bundleVersionMap.get(bundleSymbolicName);
+								if(bundleNewVersionProperties.containsKey(bundleSymbolicName)) {
+									final String newVersion = bundleNewVersionProperties.getProperty(bundleSymbolicName);
+									unitElement.getAttributes().getNamedItem("version").setNodeValue(newVersion);
+									int upgraded = newVersion.equals(oldVersion)?0:newVersion.compareTo(oldVersion)>0?1:-1;
+									LOGGER.info(format("bundle{0,choice,0# |1#\u2191|2#?} {1} : {2} \u2794 {3}", upgraded, bundleSymbolicName, oldVersion, newVersion));
+								} else if (synonymsMap.containsKey(bundleSymbolicName) && bundleNewVersionProperties.containsKey(synonymsMap.get(bundleSymbolicName))) {
+									final String newBundleSymbolicName = synonymsMap.get(bundleSymbolicName);
+									final String newVersion = bundleNewVersionProperties.getProperty(newBundleSymbolicName);
+									unitElement.getAttributes().getNamedItem("id").setNodeValue(newBundleSymbolicName);
+									unitElement.getAttributes().getNamedItem("version").setNodeValue(newVersion);
+									int upgraded = newVersion.equals(oldVersion)?0:newVersion.compareTo(oldVersion)>0?1:-1;
+									LOGGER.info(format("bundle{0,choice,0# |1#\u2191|2#?} {1} \u2794 {2} : {3} \u2794 {4}", upgraded, bundleSymbolicName, newBundleSymbolicName, oldVersion, newVersion));
+								} else {
+									missingMap.put(bundleSymbolicName, unitElement);
+									unitElement.getParentNode().removeChild(unitElement);
+								}
+							}
+						} else if(pass == 2){
+							// 2nd pass : lookup missing bundles in other locations and possibly move them to the new location
+							final Iterator<Entry<String, Element>> iterator = missingMap.entrySet().iterator();
+							while(iterator.hasNext()) {
+								final Entry<String, Element> entry = iterator.next();
+								final String bundleSymbolicName = entry.getKey();
+								if(bundleNewVersionProperties.containsKey(bundleSymbolicName)) {
+									final String newVersion = bundleNewVersionProperties.getProperty(bundleSymbolicName);
+									final Element unitElement = entry.getValue();
+									final String oldVersion = unitElement.getAttributes().getNamedItem("version").getNodeValue();
+									unitElement.getAttributes().getNamedItem("version").setNodeValue(newVersion);
+									int upgraded = newVersion.equals(oldVersion)?0:newVersion.compareTo(oldVersion)>0?1:-1;
+									
+									// move unit to new location
+									final Text newText = (Text) locationElement.getPreviousSibling().cloneNode(true);
+									newText.appendData("\t");
+									locationElement.insertBefore(unitElement, repositoryNode);
+									locationElement.insertBefore(newText, repositoryNode);
+									//
+									LOGGER.info(format("bundle{0,choice,0# |1#\u2191|2#?} {1} : {2} \u2794 {3}", upgraded, bundleSymbolicName, oldVersion, newVersion));
+									iterator.remove();
+								}
+							}
+						}
+					}
+
+					repositoryNode.getAttributes().getNamedItem("location").setNodeValue(newRepositoryUrl);
 				}
-
-				// insert a new unit
-				final Element newUnitElement = (Element) unitElementMap.entrySet().iterator().next().getValue().cloneNode(true);
-				newUnitElement.getAttributes().getNamedItem("id").setNodeValue("test");
-				newUnitElement.getAttributes().getNamedItem("version").setNodeValue("1.0.0");
-				final Text newText = (Text) locationElement.getPreviousSibling().cloneNode(true);
-				newText.appendData("\t");
-				locationElement.insertBefore(newUnitElement, repositoryNode);
-				locationElement.insertBefore(newText, repositoryNode);
-
-
-				repositoryNode.getAttributes().getNamedItem("location").setNodeValue(newRepositoryUrl);
 			}
 		}
 		
