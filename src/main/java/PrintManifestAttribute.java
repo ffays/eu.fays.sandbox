2a8909ef08b4bb1bab1f6827c134866737add780
Frederic Fays <frederic@fays.eu>
2022-12-13 14:06:09 +0100

    PrintManifestAttribute println

--- src/main/java/PrintManifestAttribute.java
+++ src/main/java/PrintManifestAttribute.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class PrintManifestAttribute {
-	public static void main(String[] args) throws IOException {
-		new java.util.jar.Manifest(System.in).getMainAttributes().getValue(System.getProperty("attribute"));
+	public static void main(String[] args) throws Exception {
+		 System.out.println(new java.util.jar.Manifest(System.in).getMainAttributes().getValue(System.getProperty("attribute")));
 	}
 }
