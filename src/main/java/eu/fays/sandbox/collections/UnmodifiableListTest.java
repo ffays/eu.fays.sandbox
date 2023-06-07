a797be97456c82a33d626b65943063b4bd23f9a5
Frederic Fays <frederic@fays.eu>
2023-06-07 13:32:41 +0200

    UnmodifiableListTest

---
+++ src/main/java/eu/fays/sandbox/collections/UnmodifiableListTest.java
@@ -0,0 +1,32 @@
+package eu.fays.sandbox.collections;
+
+import static java.util.Collections.unmodifiableList;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class UnmodifiableListTest {
+
+	
+	@Test
+	public void unmodifiableListTest() {
+		final List<String> arrayList = new ArrayList<>();
+		final List<String> linkedList = new LinkedList<>();
+		final List<String> unmodifiableArrayList = unmodifiableList(arrayList);
+		final List<String> unmodifiableLinkedList = unmodifiableList(linkedList);
+		final List<String> unmodifiableArrayList2 = unmodifiableList(unmodifiableArrayList);
+		final List<String> unmodifiableLinkedList2 = unmodifiableList(unmodifiableLinkedList);
+		
+		
+		assertFalse(arrayList == unmodifiableList(arrayList));
+		assertFalse(linkedList == unmodifiableList(linkedList));
+		assertTrue(unmodifiableArrayList == unmodifiableArrayList2);
+		assertTrue(unmodifiableLinkedList == unmodifiableLinkedList2);
+	}
+
+}
