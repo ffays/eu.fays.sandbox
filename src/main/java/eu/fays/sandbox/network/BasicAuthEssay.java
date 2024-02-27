0fe373f60012facae5efdaa1454686b7c8f1af93
Frédéric Fays <frederic@fays.eu>
2024-02-27 11:29:22 +0100

    Improved BasicAuthEssay

--- src/main/java/eu/fays/sandbox/network/BasicAuthEssay.java
+++ src/main/java/eu/fays/sandbox/network/BasicAuthEssay.java
@@ -12,8 +12,7 @@ import java.net.URL;
 import java.util.Base64;
 import java.util.Map;
 import java.util.function.Supplier;
-
-public class BasicAuthEssay {
+import java.util.logging.Logger;
 
 /*
 htpasswd -c .htpasswd sesame
@@ -26,7 +25,12 @@ Require valid-user
 EOF
 */
 
-	public static void main(String[] args) throws IOException {
+public class BasicAuthEssay {
+
+	/** Standard Logger */
+	private static final Logger LOGGER = Logger.getLogger(BasicAuthEssay.class.getName());
+
+	public static void main(String[] args) {
 		final Map<String, String> map = Map.of("p1", "alpha", "p2", "bravo", "p3", "charlie");
 		final String query = map.entrySet().stream().map(e -> format("{0}={1}", e.getKey(), encode(e.getValue(), UTF_8))).collect(joining("&"));
 		
@@ -40,13 +44,20 @@ EOF
 		final URL url = ((Supplier<URL>) ()-> {try {return new URL("https://fays.eu/?" + query);} catch (MalformedURLException e) {throw new RuntimeException(e);}}).get();
 		// @formatter:on
 
-		final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestProperty("Authorization", authorizationHeader);
-
-		// "Authorization"
-		System.out.println(url.toString());
-        System.out.println(connection.getResponseCode());
-        System.out.println(connection.getResponseMessage());
+		try {
+			final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setConnectTimeout(5000); // 5 seconds time-out to connect
+			connection.setReadTimeout(5000); // 5 seconds time-out to read connect
+			connection.setRequestProperty("Authorization", authorizationHeader);
+			final int rc = connection.getResponseCode();
+			if(rc != 200) {
+				LOGGER.warning(format("Cannot connect to {0} ({1,number,0} - {2})!", url, rc, connection.getResponseMessage())); //$NON-NLS-1$
+			} else {
+				LOGGER.info(format("Connected to {0} ({1,number,0} - {2})!", url, rc, connection.getResponseMessage())); //$NON-NLS-1$
+			}
+		} catch (final IOException e) {
+			LOGGER.warning(format("Cannot connect to {0} ({1} - {2})!", url, e.getClass().getSimpleName(), e.getMessage())); //$NON-NLS-1$
+		}
     }
 
 }
