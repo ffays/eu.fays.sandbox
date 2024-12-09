56a5d3daef8dbab1d1a4ae47a9511da465161fcb
Frédéric Fays <frederic@fays.eu>
2024-12-09 18:09:18 +0100

    OrganizationallyUniqueIdentifierLookup

---
+++ src/main/java/OrganizationallyUniqueIdentifierLookup.java
@@ -0,0 +1,62 @@
+import static java.lang.System.arraycopy;
+import static java.net.NetworkInterface.getNetworkInterfaces;
+import static java.nio.ByteBuffer.wrap;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.TreeMap;
+import java.util.zip.GZIPInputStream;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class OrganizationallyUniqueIdentifierLookup {
+
+	@SuppressWarnings({ "nls" })
+	public static void main(String[] args) throws Exception {
+		// Wireshark manufacturer database
+		final URL url = new URL("https://www.wireshark.org/download/automated/data/manuf.gz");
+		final HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+		final TreeMap<Long,String> lookup = new TreeMap<>();
+		try(final InputStream is = connection.getInputStream(); final GZIPInputStream gzis = new GZIPInputStream(is); final InputStreamReader isr = new InputStreamReader(gzis, UTF_8); final BufferedReader reader = new BufferedReader(isr)) {
+			for (String l = reader.readLine(); l != null; l = reader.readLine()) {
+				if(!l.isEmpty() && l.charAt(0) != '#') {
+					long oui = 0L;
+					for(int i=0; i<6; i++) {
+						char c = l.charAt(7 - i - (i>>1));
+						long v = c & 0xF;
+						if((c & 0x40) > 0) {
+							v += 9;
+						}
+						oui += v << (i << 2);
+					}
+					final String manufacturer = l.substring(l.lastIndexOf('\t'));
+					lookup.put(oui, manufacturer);
+				}
+			}
+		}
+
+		final Enumeration<NetworkInterface> e = getNetworkInterfaces();
+		while(e.hasMoreElements()) {
+			final NetworkInterface ni = e.nextElement();
+			final byte[] ha = ni.getHardwareAddress();
+			if(ha != null) {
+				final byte[] ha8 = new byte[8];
+				arraycopy(ha, 0, ha8, 2, ha.length);
+				final Long mac = wrap(ha8).getLong();
+				System.out.print(Long.toHexString(mac));
+				
+				final Long oui = mac >> 24;
+				if(lookup.containsKey(oui)) {
+					System.out.print('\t');
+					System.out.println(lookup.get(oui));
+				}
+				System.out.println();
+			}
+		}
+	}
+}
