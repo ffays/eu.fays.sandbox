bc4ef1340967564e14e7dbacd6951ef233a2ec7b
Frédéric Fays <frederic@fays.eu>
2025-01-16 10:22:11 +0100

    removed final keywords

--- src/main/java/Whoami.java
+++ src/main/java/Whoami.java
@@ -11,9 +11,9 @@ java.util.logging.SimpleFormatter.format=%1$tF %1$tT\t%4$s\t%3$s\t%5$s%6$s%n
 
 public class Whoami {
 	public static void main(String[] args) throws Exception {
-		final ProcessBuilder processBuilder = new ProcessBuilder("whoami");
-		final Process process = processBuilder.start();
-		final java.io.InputStream in = process.getInputStream();
+		ProcessBuilder processBuilder = new ProcessBuilder("whoami");
+		Process process = processBuilder.start();
+		java.io.InputStream in = process.getInputStream();
 		java.io.ByteArrayOutputStream os = new java.io.ByteArrayOutputStream();
 		for (int c = in.read(); c != -1; c = in.read()) os.write(c);
 		java.util.logging.Logger logger = java.util.logging.Logger.getLogger("com.example.whoami");
