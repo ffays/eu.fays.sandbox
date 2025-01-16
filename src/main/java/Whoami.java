249e679462643bd28282c1031cba8a3d322a0b88
Frédéric Fays <frederic@fays.eu>
2025-01-16 10:20:27 +0100

    Whoami

---
+++ src/main/java/Whoami.java
@@ -0,0 +1,22 @@
+// java -cp . -Djava.util.logging.config.file=jul.properties Whoami
+
+/* jul.properties
+
+handlers=java.util.logging.ConsoleHandler
+.level=FINE
+java.util.logging.ConsoleHandler.level=FINE
+java.util.logging.SimpleFormatter.format=%1$tF %1$tT\t%4$s\t%3$s\t%5$s%6$s%n
+
+*/
+
+public class Whoami {
+	public static void main(String[] args) throws Exception {
+		final ProcessBuilder processBuilder = new ProcessBuilder("whoami");
+		final Process process = processBuilder.start();
+		final java.io.InputStream in = process.getInputStream();
+		java.io.ByteArrayOutputStream os = new java.io.ByteArrayOutputStream();
+		for (int c = in.read(); c != -1; c = in.read()) os.write(c);
+		java.util.logging.Logger logger = java.util.logging.Logger.getLogger("com.example.whoami");
+		logger.fine(os.toString());
+	}
+}
