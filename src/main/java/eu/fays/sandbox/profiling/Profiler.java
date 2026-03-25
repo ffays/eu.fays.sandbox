262b1dd1a80e8fd4e94fc2802fc9d16b5b4d8530
Frédéric Fays <frederic@fays.eu>
2026-03-25 11:27:11 +0100

    Profiler WIP

--- src/main/java/eu/fays/sandbox/profiling/Profiler.java
+++ src/main/java/eu/fays/sandbox/profiling/Profiler.java
@@ -13,6 +13,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
 import jdk.jfr.Recording;
@@ -26,8 +27,7 @@ public class Profiler implements Runnable, UncaughtExceptionHandler, ThreadFacto
 	private final ScheduledExecutorService executorService;
 
 	/** Recording of the profiler */
-	private Recording recording = null;
-	
+	private final AtomicReference<Recording> recordingReference;
 	
 	public static void main(String[] args) throws InterruptedException {
 		LOGGER.info("Starting");
@@ -44,7 +44,9 @@ public class Profiler implements Runnable, UncaughtExceptionHandler, ThreadFacto
 		dump();
 		if(executorService == null) {
 			// Periodic run
-			recording = new Recording();
+			recordingReference.set(new Recording());
+			final Recording recording = recordingReference.get();
+			assert recording != null;
 			recording.start();
 		} else {
 			// Shutdown Hook 
@@ -53,24 +55,25 @@ public class Profiler implements Runnable, UncaughtExceptionHandler, ThreadFacto
 				executorService.awaitTermination(5L, SECONDS);
 			} catch (final InterruptedException e) {
 				// Do nothing
+			} finally {
 				executorService.shutdownNow();
 			}
 		}
 	}
 
-	
 	/**
 	 * Dump the recording to the file system and stop it.
 	 */
 	private void dump() {
-		assert recording != null;
 		try {
+			final Recording recording = recordingReference.get();
+			assert recording != null;
 			final String format = executorService !=null ? "yyyy-MM-dd_HH\u00F7mm\u00F7ss'-final.jfr'":"yyyy-MM-dd_HH\u00F7mm\u00F7ss'.jfr'";
 			final String filename = DateTimeFormatter.ofPattern(format).format(now());
 			final Path path = Paths.get(filename);
-			recording.dump(path);
 			recording.stop();
-			recording = null;
+			recording.dump(path);
+			recording.close();
 		} catch (final IOException e) {
 			LOGGER.log(SEVERE, e.getMessage(), e);
 		}
@@ -87,24 +90,44 @@ public class Profiler implements Runnable, UncaughtExceptionHandler, ThreadFacto
 		assert unit != null;
 		//
 		this.executorService = null;
+		this.recordingReference = new AtomicReference<>(new Recording());
 		//
-		final ScheduledExecutorService executorService = newSingleThreadScheduledExecutor(this);
+		
+		final Recording recording = recordingReference.get();
+		recording.start();
+		
+		final Profiler threadFactory = new Profiler(); // as both Thread Factory and Uncaught Exception Handler
+		final ScheduledExecutorService executorService = newSingleThreadScheduledExecutor(threadFactory);  
 		executorService.scheduleAtFixedRate(this, period, period, unit);
-		new Profiler(executorService); // as Shutdown Hook
+		
+		// Shutdown Hook
+		final Profiler shutdownHookRunnable = new Profiler(executorService, recordingReference); // as Shutdown Hook
+		final Thread shutdownHook = new Thread(shutdownHookRunnable);
+		shutdownHook.setName(getClass().getSimpleName() + "ShutdownHook");
+		Runtime.getRuntime().addShutdownHook(shutdownHook);
 	}
 	
 	/**
-	 * Constructor for the Shutdown Hook
-	 * @param executorService executor service
+	 * Constructor for both {@link ThreadFactory} and {@link UncaughtExceptionHandler}
 	 */
-	private Profiler(final ScheduledExecutorService executorService) {
+	private Profiler() {
+		this.executorService = null;
+		this.recordingReference = null;
+	}
+	
+	/**
+	 * Constructor for the {@link Runtime#addShutdownHook(Thread) Shutdown Hook}
+	 * @param executorService executor service
+	 * @param recordingReference pointer to effective {@link Recording}
+	 */
+	private Profiler(final ScheduledExecutorService executorService, AtomicReference<Recording> recordingReference) {
 		//
 		assert executorService != null;
+		assert recordingReference != null;
+		assert recordingReference.get() != null;
 		//
 		this.executorService = executorService;
-		final Thread shutdownHook = new Thread(this);
-		shutdownHook.setName(getClass().getSimpleName() + "ShutdownHook");
-		Runtime.getRuntime().addShutdownHook(shutdownHook);
+		this.recordingReference = recordingReference;
 	}
 	
 	/**
@@ -122,11 +145,9 @@ public class Profiler implements Runnable, UncaughtExceptionHandler, ThreadFacto
 	public Thread newThread(final Runnable runnable) {
 		//
 		assert runnable != null;
-		assert runnable == this;
-		assert recording == null;
+		assert executorService == null;
+		assert recordingReference == null;
 		//
-		recording = new Recording();
-		recording.start();
 		
 		final Thread result = new Thread(runnable);
 		result.setName(getClass().getSimpleName());
